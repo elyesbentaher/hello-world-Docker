8fe3e1c698283ef040b7689b882a940b33d6b2eb
root <root@instance-1.europe-west1-b.c.carbide-ego-249613.internal>
2023-11-07 15:16:47 +0000

    add html file

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+# Use the official Node.js image as the base image
+FROM node:14
+
+# Set the working directory in the container
+WORKDIR /usr/src/app
+
+# Install OpenJDK
+RUN apt-get update && apt-get install -y openjdk-11-jdk
+
+# Install Node.js dependencies
+COPY package*.json ./
+RUN npm install
+
+# Bundle your source code inside the Docker image
+COPY . .
+
+# Expose the port that the application uses
+EXPOSE 3000
+
+# Set the JAVA_HOME environment variable
+ENV JAVA_HOME /usr/lib/jvm/java-11-openjdk-amd64
+
+
+# Command to run your application
+CMD ["./gradlew", "buildNode"]
+
--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Hello, World!');
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+          if (req.url === '/') {
+                      fs.readFile('index.html', (err, data) => {
+                                    if (err) {
+                                                    res.writeHead(500, { 'Content-Type': 'text/plain' });
+                                                    res.end('Internal Server Error');
+                                                  } else {
+                                                                  res.writeHead(200, { 'Content-Type': 'text/html' });
+                                                                  res.end(data);
+                                                                }
+                                  });
+                    }
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+          console.log(`Server is running on port ${PORT}`);
+});
